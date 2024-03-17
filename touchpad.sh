85887125a8b52c568c123f2f0ee1cde61d6a3ded
Himanshu Soni <sonihimanshu934@gmail.com>
2024-03-17 22:26:23 +0530

    root commit

---
+++ touchpad.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+xinput set-prop "DELL0844:00 04F3:3074 Touchpad" "libinput Tapping Enabled" 1
+xinput set-prop "DELL0844:00 04F3:3074 Touchpad" "317" 1
+xinput set-prop "DELL0844:00 04F3:3074 Touchpad" "libinput Accel Speed" 0.7
