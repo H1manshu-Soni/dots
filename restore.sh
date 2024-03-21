d48cb5f368bd82cc5bfb27ab1636fd0bedee06f0
H1manshu-$oni <84933847+H1manshu-Soni@users.noreply.github.com>
2024-03-22 00:51:13 +0530

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -4,7 +4,7 @@
 echo "windowrulev2 = opacity 0.80 0.80,class:^(org.gnome.clocks)$"  >> $HOME/.config/hypr/windowrules.conf
 echo "windowrulev2 = opacity 0.80 0.80,class:^(org.gnome.Calculator)$"  >> $HOME/.config/hypr/windowrules.conf
 echo "windowrulev2 = opacity 0.80 0.80,class:^(org.telegram.desktop)$" >> $HOME/.config/hypr/windowrules.conf
-echo "windowrulev2 = opacity 0.80 0.80,class:^(obsidian)$" >> $HOME/.config/hypr/windowrules.config
+echo "windowrulev2 = opacity 0.80 0.80,class:^(obsidian)$" >> $HOME/.config/hypr/windowrules.conf
 echo "windowrulev2 = opacity 0.80 0.80,class:^(sublime_text)$" >>$HOME/.config/hypr/windowrules.conf
 
 #echo "windowrulev2 = float, class:^()$" >> $HOME/.config/hypr/windowrules.conf
